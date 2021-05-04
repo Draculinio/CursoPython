8a26354a0773ba446a684ed80fd05a5a9b647e09
Pablo Soifer <pablo.soifer@wwecorp.com>
2021-05-04 16:19:13 -0300

    Clase 5

--- diccionario.py
+++ diccionario.py
@@ -1,7 +1,35 @@
 jugador = {
     'nombre': 'Ruben',
     'apellido': 'Paz',
-    'club' : ['Racing', 'Peñarol']
 }
 
-print(jugador['nombre'])
+
+#jugadores = [
+
+    #{'nombre': 'algo',
+    #'apellido':'algo'},
+    #{'nombre': 'algo2',
+    #'apellido':'algo2'}
+
+#]
+
+otros_clubes = {
+
+    'clubes': ['Racing', 'Peñarol','Internacional','Racing París', 'Genoa']
+
+}
+
+jugador.update(otros_clubes)
+
+jugador['goles'] = 150
+
+jugador['goles'] = 200
+
+print(jugador)
+
+print(jugador.values())
+
+print(jugador.keys())
+
+jugador.clear()
+print(jugador)
