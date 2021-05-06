e9fa977057caf5e1a480a7a68669610c1c2d2ea6
Pablo Soifer <pablo.soifer@wwecorp.com>
2021-05-06 15:15:10 -0300

    Clase 6

---
+++ tarea6.py
@@ -0,0 +1,15 @@
+#Crear una clase persona
+#nombre
+#Apellido
+#edad
+#dni
+#METODOS:
+#nombrar -> cArlos -> Carlos
+#dar apellido -> suarez -> Suarez
+#cumplir -> Cumplir años
+#Gustos de ropa -> 0 y 9 años le gusta la moda infantil
+#->10 y 18 le gusta la ropa juvenil
+#->19 y 30 Ropa informal
+#->31 y 40 Ropa formal
+#->41 a 50 Ropa de crisis de la mediana edad
+#Más de 50 boinas y vestidos floreados
